0a340adf9229dc6028e508597e4f0f7e511708fe
CJovan02 <161864859+CJovan02@users.noreply.github.com>
2024-03-07 16:44:03 +0100

    - Za sve kontrolere dodate GetByEmail, i preko toga provera u post metodu na osnovu jmbg/pib i email-a - Dodat folder Utility gde je sad smestena funkcija za proveru formata email-a,   tu su jos dodate funkcije koje proveravaju korisnika medjutim za sada to nema primenu - Funkcije za proveru mogu da budu i u klasama za model, medjutim email posto je svima isti format,   moze da bude zaseban - Funkcije u Utility su koriscene za proveru u get put i post metodama (za email, pib i jmbg)
    --PROBLEM: 2 korisnika ne mogu da imaju isti jmbg, ali majstor i korisnik mogu, isto to vazi i za email

--- MajstorHUB-Back/MajstorHUB/Services/IFirmaService.cs
+++ MajstorHUB-Back/MajstorHUB/Services/IFirmaService.cs
@@ -5,6 +5,7 @@ public interface IFirmaService
     Task<List<Firma>> GetAll();
     Task<Firma> GetById(string id);
     Task<Firma> GetByPib(string pib);
+    Task<Firma> GetByEmail(string email);
     Task Create(Firma firma);
     Task Update(string id, Firma firma);
     Task Delete(string id);
--- MajstorHUB-Back/MajstorHUB/Services/IKorisnikService.cs
+++ MajstorHUB-Back/MajstorHUB/Services/IKorisnikService.cs
@@ -5,6 +5,7 @@ public interface IKorisnikService
     Task<List<Korisnik>> GetAll();
     Task<Korisnik> GetById(string id);
     Task<Korisnik> GetByJmbg(string jmbg);
+    Task<Korisnik> GetByEmail(string email);
     Task Create(Korisnik korisnik);
     Task Update(string id, Korisnik korisnik);
     Task Delete(string id);
